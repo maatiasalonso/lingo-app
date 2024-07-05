00f6f0a3b92a8ff4fb00e3829c01c44339683460
maatiasalonso <matias.a.sanmartin@gmail.com>
2024-07-05 02:00:10 -0400

    chore: Add UserProgress component for displaying user progress in LearnPage

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,11 +1,46 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Button } from "@/components/ui/button";
+import { UserProgress } from "@/components/user-progress";
+import { IconArrowLeft } from "@tabler/icons-react";
+import Link from "next/link";
 
 export default function LearnPage() {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
-      <StickyWrapper>LearnPage</StickyWrapper>
-      <FeedWrapper>FeedWrapper</FeedWrapper>
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{
+            title: "Spanish",
+            language: "ES",
+          }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
     </div>
   );
 }
+
+type HeaderProps = {
+  title: string;
+};
+
+const Header = ({ title }: HeaderProps) => {
+  return (
+    <div className="sticky top-0 bg-white lg:pt-[28px] lg:mt-[-28px] pb-3 flex items-center border-b-2 mb-5 text-zinc-400 lg:z-50">
+      <Link href={"/courses"}>
+        <Button variant={"ghost"} size={"sm"}>
+          <IconArrowLeft className="h-5 w-5 stroke-2 text-zinc-400" />
+        </Button>
+      </Link>
+      <div className="flex-grow flex justify-center">
+        <h1 className="text-lg font-bold">{title}</h1>
+      </div>
+    </div>
+  );
+};
---
+++ components/user-progress.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@/components/ui/button";
+import { IconInfinity } from "@tabler/icons-react";
+import Image from "next/image";
+import Link from "next/link";
+import ReactCountryFlag from "react-country-flag";
+
+type Props = {
+  activeCourse: { title: string; language: string };
+  hearts: number;
+  points: number;
+  hasActiveSubscription: boolean;
+};
+
+export const UserProgress = ({
+  activeCourse,
+  hearts,
+  points,
+  hasActiveSubscription,
+}: Props) => {
+  const { title, language } = activeCourse;
+  return (
+    <div className="flex items-center justify-between gap-x-2 w-full">
+      <Link href={"/courses"}>
+        <Button variant={"ghost"} className="flex gap-x-2 items-center">
+          <ReactCountryFlag
+            svg
+            countryCode={language}
+            style={{
+              width: "2em",
+              height: "2em",
+              borderRadius: "33%",
+            }}
+          />
+        </Button>
+      </Link>
+      <Link href={"/shop"}>
+        <Button
+          variant={"ghost"}
+          className="text-orange-500 flex gap-x-2 items-center"
+        >
+          <Image src={"/points.svg"} alt="Points" width={28} height={28} />
+          {points}
+        </Button>
+      </Link>
+      <Link href={"/shop"}>
+        <Button
+          variant={"ghost"}
+          className="text-rose-500 flex gap-x-2 items-center"
+        >
+          <Image src={"/heart.svg"} alt="Hearts" width={22} height={22} />
+          {hasActiveSubscription ? (
+            <IconInfinity className="h-4 w-4 stroke-3" />
+          ) : (
+            hearts
+          )}
+        </Button>
+      </Link>
+    </div>
+  );
+};
