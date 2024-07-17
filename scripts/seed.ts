183073e86307db65397b30e76c32d0beb8b7bb7c
maatiasalonso <matias.a.sanmartin@gmail.com>
2024-07-17 01:06:01 -0400

    chore: Update database schema, add tables and relations for units, lessons, and challenges

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -10,48 +10,179 @@ const sql = neon(process.env.DATABASE_URL!);
 
 const db = drizzle(sql, { schema });
 
+const IDS = {
+  SPANISH: 1,
+  FRENCH: 2,
+  GERMAN: 3,
+  ITALIAN: 4,
+};
+
+const insertUsers = async () => {
+  const users = [
+    {
+      id: 1,
+      name: "Test user",
+      email: "test@test.com",
+      password: bcrypt.hashSync("password", 10),
+      image: "https://randomuser.me/api/portraits",
+    },
+    {
+      id: 2,
+      name: "Test user 2",
+      email: "mail@mail.com",
+      password: bcrypt.hashSync("password", 10),
+      image: "https://randomuser.me/api/portraits",
+    },
+  ];
+
+  await db.insert(schema.users).values(users);
+};
+
+const insertCourses = async () => {
+  const courses = [
+    {
+      id: IDS.SPANISH,
+      title: "Spanish",
+      language: "ES",
+    },
+    {
+      id: IDS.FRENCH,
+      title: "French",
+      language: "FR",
+    },
+    {
+      id: IDS.GERMAN,
+      title: "German",
+      language: "DE",
+    },
+    {
+      id: IDS.ITALIAN,
+      title: "Italian",
+      language: "IT",
+    },
+  ];
+
+  await db.insert(schema.courses).values(courses);
+};
+
+const insertUnits = async () => {
+  const units = [
+    {
+      id: 1,
+      courseId: IDS.SPANISH,
+      title: "Unit 1",
+      description: "Learn the basics of Spanish",
+      order: 1,
+    },
+  ];
+  await db.insert(schema.units).values(units);
+};
+
+const insertLessons = async () => {
+  const lessons = [
+    {
+      id: 1,
+      unitId: 1,
+      title: "Nouns",
+      order: 1,
+    },
+    {
+      id: 2,
+      unitId: 1,
+      title: "Verbs",
+      order: 2,
+    },
+    {
+      id: 3,
+      unitId: 1,
+      title: "Adjectives",
+      order: 3,
+    },
+  ];
+
+  await db.insert(schema.lessons).values(lessons);
+};
+
+const insertChallenges = async () => {
+  const challenges = [
+    {
+      id: 1,
+      lessonId: 1,
+      type: "SELECT" as const,
+      order: 1,
+      question: `Which one of these is "the man"?`,
+    },
+  ];
+
+  await db.insert(schema.challenges).values(challenges);
+};
+
+const insertChallengeOptions = async () => {
+  const challengeOptions = [
+    {
+      id: 1,
+      challengeId: 1,
+      imageSrc: "/man.svg",
+      isCorrect: true,
+      text: "El hombre",
+      audioSrc: "/es_man.mp3",
+    },
+    {
+      id: 2,
+      challengeId: 1,
+      imageSrc: "/woman.svg",
+      isCorrect: false,
+      text: "La mujer",
+      audioSrc: "/es_woman.mp3",
+    },
+    {
+      id: 3,
+      challengeId: 1,
+      imageSrc: "/robot.svg",
+      isCorrect: false,
+      text: "El robot",
+      audioSrc: "/es_robot.mp3",
+    },
+  ];
+
+  await db.insert(schema.challengeOptions).values(challengeOptions);
+};
+
+const deleteExistingData = async () => {
+  await db.delete(schema.courses);
+  await db.delete(schema.userProgress);
+  await db.delete(schema.units);
+  await db.delete(schema.lessons);
+  await db.delete(schema.challenges);
+  await db.delete(schema.challengeProgress);
+  await db.delete(schema.challengeOptions);
+  await db.delete(schema.users);
+};
+
 const main = async () => {
   try {
     console.log("Seeding database...");
 
-    await db.delete(schema.courses);
-    await db.delete(schema.users);
-    await db.delete(schema.userProgress);
-
-    console.log("Inserting courses...");
-    await db.insert(schema.courses).values([
-      {
-        id: 1,
-        title: "Spanish",
-        language: "ES",
-      },
-      {
-        id: 2,
-        title: "French",
-        language: "FR",
-      },
-      {
-        id: 3,
-        title: "German",
-        language: "DE",
-      },
-      {
-        id: 4,
-        title: "Italian",
-        language: "IT",
-      },
-    ]);
+    console.log("Deleting existing data...");
+    await deleteExistingData();
 
     console.log("Inserting users...");
-    await db.insert(schema.users).values([
-      {
-        id: 1,
-        name: "Test user",
-        email: "test@test.com",
-        password: bcrypt.hashSync("password", 10),
-        image: "https://randomuser.me/api/portraits",
-      },
-    ]);
+    await insertUsers();
+
+    console.log("Inserting courses...");
+    await insertCourses();
+
+    console.log("Inserting units...");
+    await insertUnits();
+
+    console.log("Inserting lessons...");
+    await insertLessons();
+
+    console.log("Inserting challenges...");
+    await insertChallenges();
+
+    console.log("Inserting challenge options...");
+    await insertChallengeOptions();
 
     console.log("Database seeded successfully");
   } catch (error) {
@@ -59,4 +190,5 @@ const main = async () => {
     throw new Error("Failed to seed database");
   }
 };
+
 main();
