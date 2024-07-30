6977b5f769ffccac00955ccfe1b4ade09b48fabb
maatiasalonso <matias.a.sanmartin@gmail.com>
2024-07-29 20:49:41 -0400

    Add button variants

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,17 +10,32 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-white text-black border-zinc-200 border-2 border-b-4 active:border-b-2 hover:bg-zinc-100 text-zinc-500",
+        primary:
+          "bg-sky-400 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0",
+        primaryOutline: "bg-white text-sky-400 hover:bg-zinc-100",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-green-500 text-primary-foreground hover:bg-green-500/90 border-green-600 border-b-4 active:border-b-0",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        secondaryOutline: "bg-white text-green-500 hover:bg-zinc-100",
+        danger:
+          "bg-rose-500 text-primary-foreground hover:bg-rose-500/90 border-rose-600 border-b-4 active:border-b-0",
+        dangerOutline: "bg-white text-rose-500 hover:bg-zinc-100",
+        super:
+          "bg-indigo-500 text-primary-foreground hover:bg-indigo-500/90 border-indigo-600 border-b-4 active:border-b-0",
+        superOutline: "bg-white text-indigo-500 hover:bg-zinc-100",
+        ghost:
+          "bg-transparent text-zinc-500 border-transparent border-0 hover:bg-zinc-100",
         link: "text-primary underline-offset-4 hover:underline",
         locked:
           "bg-zinc-200 text-primary-foreground hover:bg-zinc-200/90 border-zinc-400 border-b-4 active:border-b-0",
+        sidebar:
+          "bg-transparent text-zinc-500 border-2 border-transparent hover:bg-zinc-100 transition-none",
+        sidebarOutline:
+          "bg-sky-500/15 text-sky-500 border-sky-300 border-2 hover:bg-sky-500/20 transition-none",
       },
       size: {
         default: "h-9 px-4 py-2",
