cd86ea6ddbc5d42ab798c8c283b2870a6ccb9c14
maatiasalonso <matias.a.sanmartin@gmail.com>
2024-08-04 23:44:53 -0400

    feat: :sparkles: Hearts
    Add logic to handle wrong answer, reducing hearts count

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -3,7 +3,9 @@
 import { auth } from "@/auth";
 import db from "@/db/drizzle";
 import { getCourseById, getUserProgress } from "@/db/queries";
-import { userProgress } from "@/db/schema";
+import { challengeProgress, challenges, userProgress } from "@/db/schema";
+import { revalidatePaths } from "@/lib/utils";
+import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
@@ -50,4 +52,62 @@ export const upsertUserProgress = async (courseId: number) => {
   revalidatePath("/courses");
   revalidatePath("/learn");
   redirect("/learn");
+};
+
+export const reduceHearts = async (challengeId: number) => {
+  const session = await auth();
+
+  if (!session?.user) {
+    throw new Error("Unauthorized");
+  }
+
+  const currentUserProgress = await getUserProgress();
+
+  const challenge = await db.query.challenges.findFirst({
+    where: eq(challenges.id, challengeId),
+  });
+
+  if (!challenge) {
+    throw new Error("Challenge not found");
+  }
+
+  const lessonId = challenge.lessonId;
+
+  const pathsToRevalidate = [
+    "/shop",
+    "/learn",
+    "/quests",
+    "/leaderboard",
+    `/lesson/${lessonId}`,
+  ];
+
+  const existingChallengeProgress = await db.query.challengeProgress.findFirst({
+    where: and(
+      eq(challengeProgress.userId, session.user.id || ""),
+      eq(challengeProgress.challengeId, challengeId),
+    ),
+  });
+
+  const isPractice = !!existingChallengeProgress;
+
+  if (isPractice) {
+    return { error: "practice" };
+  }
+
+  if (!currentUserProgress) {
+    throw new Error("User progress not found");
+  }
+
+  if (currentUserProgress.hearts === 0) {
+    return { error: "hearts" };
+  }
+
+  await db
+    .update(userProgress)
+    .set({
+      hearts: Math.max(currentUserProgress.hearts - 1, 0),
+    })
+    .where(eq(userProgress.userId, session.user.id || ""));
+
+  revalidatePaths(pathsToRevalidate);
 };
